d1557244d22d20a5c2bf2e421909e50c5a41c6e6
Diego Rodriguez <diegorodriguezk@MacBook-Air-de-Diego.local>
2021-10-23 11:20:50 -0400

    amazon_scraper

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const Apikey = '8cf480f37ce3e43f3178411d9c71e5df';
+const BaseURL =`http://api.scraperapi.com?api_key=${Apikey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Amazon Scraper API');
+
+});
+
+//GET Product Details
+
+app.get('/products/:ProductId', async(req, res) => {
+  const { ProductId } = req.params;
+  try {
+    const response = await request(`${BaseURL}&url=https://www.amazon.com/dp/${ProductId}`);
+
+    res.json(JSON.parseresponse);
+  }  catch(error) {
+    res.json(error)
+  }
+});
+
+app.listen (PORT, () => console.log('Server running on port ${PORT}') );
